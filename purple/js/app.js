8044bfb7e9dbacb83b6650864ffff240588ce127
LewisLindleySmith <fao-lewis-smith@hotmail.com>
2020-05-20 15:28:23 +0100

    hide button js finished

--- purple/js/app.js
+++ purple/js/app.js
@@ -1,10 +1,19 @@
 // Click event
-//
 var hideBtn = document.getElementById("hideBtn");
 var buyerDetails = document.getElementById("buyerDetails");
+var chevronUp = document.getElementById("chevronUp");
+var test = document.getElementById("test");
 
 hideBtn.addEventListener("click", function () {
   buyerDetails.classList.toggle("active");
+
+  chevronUp.classList.toggle("rotated");
+
+  if (test.innerHTML === "show") {
+    test.innerHTML = "hide";
+  } else {
+    test.innerHTML = "show";
+  }
 
   // Text change
   // Toggle btn class "open"
