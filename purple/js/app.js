c79e3cd5ca7d5cd7221d8d4a116eaa1d7d7c5f6e
LewisLindleySmith <fao-lewis-smith@hotmail.com>
2020-06-01 10:26:22 +0100

    modal comments box complete

--- purple/js/app.js
+++ purple/js/app.js
@@ -18,3 +18,35 @@ hideBtn.addEventListener("click", function () {
   // Text change
   // Toggle btn class "open"
 });
+
+// modal window
+// get modal element
+var modal = document.getElementById("commentsModal");
+// get open modal button
+var modalBtn = document.getElementById("modalBtn");
+// get close modal button
+var closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+// Listen for open click
+modalBtn.addEventListener("click", openModal);
+// listen for close click
+closeBtn.addEventListener("click", closeModal);
+// Listen for outside click
+window.addEventListener("click", clickOutside);
+
+// function to open modal
+function openModal() {
+  modal.style.display = "block";
+}
+
+// function to close modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// function to close modal if outside click
+function clickOutside(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
